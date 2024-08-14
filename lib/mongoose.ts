d39e9a778a9db53f7ed2c3a0beb85bf280eb6e18
nayon117 <hasibul.nayon1@gmail.com>
2024-08-14 10:27:45 +0600

    connect to mongodb

---
+++ lib/mongoose.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+let isConnected:boolean = false;
+
+export const connectToDatabase = async () =>{
+    mongoose.set('strictQuery',true);
+
+    if(!process.env.MONGODB_URI) return console.log('MONGODB_URI not found');
+
+    if(isConnected) return console.log('Already connected to database');
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,{
+            dbName:'tournamax'
+        })
+        isConnected = true;
+        console.log('Connected to database');
+        
+    } catch (error) {
+        console.log('Error connecting to database',error);   
+    }
+}
